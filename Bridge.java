f885f41c5220f97e82a2a2d25f99daa67ff9c444
Eesha Anjum <84733993+eeshaanjum@users.noreply.github.com>
2025-02-03 19:05:38 -0500

    Update Bridge.java

--- Bridge.java
+++ Bridge.java
@@ -1,13 +1,11 @@
-/* Copyright 2012 David Pearson.
- * BSD License.
- */
+
 
 import java.util.ArrayList;
 
 /**
  * A representation of a bridge on a Hex board.
  *
- * @author David Pearson
+ * 
  */
 public class Bridge {
 	public Location l1;
