93398018203015f713a053791b112a82039da35d
Eesha Anjum <84733993+eeshaanjum@users.noreply.github.com>
2025-02-03 19:07:18 -0500

    Update TreeNode.java

--- TreeNode.java
+++ TreeNode.java
@@ -1,15 +1,7 @@
-/* Copyright 2012 David Pearson.
- * BSD License.
- */
+
 
 import java.util.ArrayList;
 
-/**
- * A node in a tree of possible moves
- * 	(which ends up being a doubly-linked list).
- *
- * @author David Pearson
- */
 public class TreeNode {
 	public ArrayList<TreeNode> children;
 	public TreeNode parent;
