4d6a512383f76bc0b37e8fab5c1745b876f2ab36
Eesha Anjum <84733993+eeshaanjum@users.noreply.github.com>
2025-02-03 19:04:55 -0500

    Update AI.java

--- AI.java
+++ AI.java
@@ -1,11 +1,10 @@
-/* Copyright 2012 David Pearson.
+/* 
  * BSD License
  */
 
 /**
  * An interface for AI players, to be implmented by various subclasses.
  *
- * @author David Pearson
  */
 public abstract class AI {
 	public abstract int getPlayerCode();
